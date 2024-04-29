8dbb7eb5275f8173f1475914e9620a2bf0580523
Paola <Paola@DESKTOP-L53JRFL>
2024-04-29 18:00:19 -0300

    Adicionar arquivos de projeto.

---
+++ diferenca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int A, B, C, D, DIFERENCA;
+
+        A = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        B = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        C = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        D = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        DIFERENCA = A * B - C * D;
+
+        Console.WriteLine("DIFERENCA = " + DIFERENCA);
+
+        Console.Read();
+
+    }
+
+}
